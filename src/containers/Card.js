a1ee9492cb6b1335b551ab3b12de3fb22ff7ac00
Thameera Senanayaka <thameera123@gmail.com>
2017-03-12 19:09:44 +0530

    Add context menu

--- src/containers/Card.js
+++ src/containers/Card.js
@@ -1,18 +1,31 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { moveLeft, moveRight } from '../actions';
+import { ContextMenu, MenuItem, ContextMenuTrigger } from 'react-contextmenu';
 import './Card.css';
 
 let Card = ({ id, label, moveLeft, moveRight }) => {
+  const handleClick = (e, data) => {
+    console.log(`${data} - ${label}`);
+  }
+
   return (
     <div className="card">
-      <div>
-        {label}
-      </div>
-      <div>
-        <button onClick={() => moveLeft(id)}>&lt;</button>
-        <button onClick={() => moveRight(id)}>&gt;</button>
-      </div>
+      <ContextMenuTrigger id={"context-menu-card-"+id}>
+        <div>
+          {label}
+        </div>
+        <div>
+          <button onClick={() => moveLeft(id)}>&lt;</button>
+          <button onClick={() => moveRight(id)}>&gt;</button>
+        </div>
+      </ContextMenuTrigger>
+
+      <ContextMenu id={"context-menu-card-"+id}>
+        <MenuItem data={{action: 'Delete'}} onClick={handleClick}>
+          Delete
+        </MenuItem>
+      </ContextMenu>
     </div>
   )
 };
