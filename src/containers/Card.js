d5efb6b8fb93fd058a74ad4426975bd93ff6f83d
Thameera Senanayaka <thameera123@gmail.com>
2017-03-12 19:37:57 +0530

    Convert Card to a class
    In prep for react-dnd

--- src/containers/Card.js
+++ src/containers/Card.js
@@ -1,35 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { moveLeft, moveRight, deleteCard } from '../actions';
 import { ContextMenu, MenuItem, ContextMenuTrigger } from 'react-contextmenu';
 import './Card.css';
 
-let Card = ({ id, label, moveLeft, moveRight, deleteCard }) => {
-  const handleClick = (e, data) => {
+class Card extends Component {
+  handleClick(e, data) {
     if (data.action === 'Delete') {
-      deleteCard(id);
+      this.props.deleteCard(this.props.id);
     }
   }
 
-  return (
-    <div className="card">
-      <ContextMenuTrigger id={"context-menu-card-"+id}>
-        <div>
-          {label}
-        </div>
-        <div>
-          <button onClick={() => moveLeft(id)}>&lt;</button>
-          <button onClick={() => moveRight(id)}>&gt;</button>
-        </div>
-      </ContextMenuTrigger>
+  render() {
+    const { id, label, moveLeft, moveRight } = this.props;
+    return (
+      <div className="card">
+        <ContextMenuTrigger id={"context-menu-card-"+id}>
+          <div>
+            {label}
+          </div>
+          <div>
+            <button onClick={() => moveLeft(id)}>&lt;</button>
+            <button onClick={() => moveRight(id)}>&gt;</button>
+          </div>
+        </ContextMenuTrigger>
 
-      <ContextMenu id={"context-menu-card-"+id}>
-        <MenuItem data={{action: 'Delete'}} onClick={handleClick}>
-          Delete
-        </MenuItem>
-      </ContextMenu>
-    </div>
-  )
+        <ContextMenu id={"context-menu-card-"+id}>
+          <MenuItem data={{action: 'Delete'}} onClick={this.handleClick.bind(this)}>
+            Delete
+          </MenuItem>
+        </ContextMenu>
+      </div>
+    )
+  }
 };
 
 const mapStateToProps = (state) => ({
